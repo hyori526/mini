b7e3e42ca10d289dc74938444009f01fa50cd501
Hyori Jang <hyori526@gmail.com>
2025-01-16 01:32:20 +0900

    첫번째 커밋

---
+++ mini_01/src/main/java/io/goorm/mini_01/Mini01Application.java
@@ -0,0 +1,13 @@
+package io.goorm.mini_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mini01Application.class, args);
+	}
+
+}
---
+++ mini_01/src/test/java/io/goorm/mini_01/Mini01ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.goorm.mini_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mini01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
